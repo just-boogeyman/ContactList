d87c6a95149bc264d5b41fbf32d60792c10e5546
Aroslav Kochkin <kochkin.2328@inbox.ru>
2023-09-24 22:38:02 +0700

    added DataStore

---
+++ ContactList/Services/DataStore.swift
@@ -0,0 +1,42 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by Ярослав Кочкин on 24.09.2023.
+//
+
+import Foundation
+
+// MARK: - DataStore Services
+
+class DataStore {
+        
+    let names = [
+        "John",
+        "Aaron",
+        "Tim",
+        "Ted",
+        "Steven"
+    ]
+    let surnames = [
+        "Smith",
+        "Dow",
+        "Isaacson",
+        "Pennyworth",
+        "Jankins"
+    ]
+    let phone = [
+        "+13979141",
+        "+32179141",
+        "+45379141",
+        "+98279141",
+        "+77779141"
+    ]
+    let email = [
+        "Smith@mail.ru",
+        "Aaron@mail.ru",
+        "Tim@mail.ru",
+        "Ted@mail.ru",
+        "Steven@mail.ru"
+    ]
+}
